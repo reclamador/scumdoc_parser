370f6698237707fb55d43663210b971dc22998f1
Juan Madurga <jlmadurga@gmail.com>
2017-11-08 12:37:40 +0100

    standarize search

--- scumdoc_parser/__init__.py
+++ scumdoc_parser/__init__.py
@@ -1,13 +1,52 @@
 # -*- coding: utf-8 -*-
 import re
 from datetime import datetime
-from difflib import get_close_matches
+from difflib import get_close_matches, SequenceMatcher
 
 """Top-level package for ScumDoc Parser."""
 
 __author__ = """Juan Madurga"""
 __email__ = 'jlmadurga@gmail.com'
 __version__ = '0.1.0'
+
+
+
+class BaseSearch(object):
+
+    def __init__(self, name, pre_process=None, post_process=None, group=None):
+        self.name = name
+        self.pre_process = pre_process
+        self.post_process = post_process
+        self.group = group
+
+    def _search(self, line):
+        raise NotImplementedError
+
+    def search(self, line):
+        return self.post_process(self._search(self.pre_process(line)))
+
+
+class RegexSearch(BaseSearch):
+
+    def __init__(self, name, regex, pre_process=None, post_process=None, group=None):
+        super(RegexSearch, self).__init__(name, regex, pre_process, post_process, group)
+        self.regex = regex
+
+    def _search(self, line):
+        return re.search(self.regex, line)
+
+class FuzzySearch(BaseSearch):
+
+    def __init__(self, name, keyword, ratio, pre_process=None, group=None):
+        super(FuzzySearch, self).__init__(name, pre_process, self._post_process, group)
+        self.ratio = ratio
+        self.keyword = keyword
+
+    def _post_process(self, search):
+        return search
+
+    def _search(self, line):
+        return SequenceMatcher(a=self.keyword, b=line).ratio() >= self.ratio
 
 
 class BaseScumDocParser(object):
@@ -19,10 +58,8 @@ class BaseScumDocParser(object):
 
     def __init__(self, raw_text):
         self.text = self._normalize(raw_text)
-        self._dates = []
         self._keywords = dict(self.KEYWORDS)
         self._ocrs = self.OCR
-        self._ocr_results = {}
 
     def _normalize(self, raw_text):
         return [line.strip().lower() for line in raw_text.splitlines() if line.strip()]
@@ -34,10 +71,10 @@ class BaseScumDocParser(object):
         return None
 
 
-    def _search_dates(self, line):
+    def _search_dates(self, line, dates_results):
         found = re.search('(\d{2})\s(\d{2})\s(\d{4})', line)
         if found:
-            self._dates.append(datetime.strptime('%s-%s-%s' % found.groups(), '%d-%m-%Y').date())
+            dates_results.append(datetime.strptime('%s-%s-%s' % found.groups(), '%d-%m-%Y').date())
 
     def _search_keywords(self, keywords=None, result=None):
         if not keywords:
@@ -50,32 +87,31 @@ class BaseScumDocParser(object):
                 result[keyword] = True
         return result
 
-    def _extract_ocr(self, line):
+    def _extract_ocr(self, line, ocr_results):
         line_normalized = line.replace(' ','')
         for ocr in self._ocrs:
             match = re.match(ocr, line_normalized)
             if match:
-                self._ocr_results.update(match.groupdict())
-                break
+                ocr_results.update(match.groupdict())
 
-
-    def _result(self):
-        result = {}
-        result.update({'keywords': self._keywords})
-        result.update({'dates': self._dates})
-        result.update({'ocr': self._ocr_results})
-        return result
+    def _extract_keywords(self, line, results):
+        for keyword in self._keywords:
+            if SequenceMatcher(a=keyword, b=line).ratio() >= 0.7:
+                results[keyword] = True
 
     def parse(self):
         """
         Parse doc text
         :return:
         """
+        keyword_results = dict((keyword, False) for keyword in self._keywords)
+        dates_results = []
+        ocr_results = {}
         for line in self.text:
-            self._search_dates(line)
-            self._extract_ocr(line)
-        self._keywords = self._search_keywords()
-        return self._result()
+            self._search_dates(line, dates_results)
+            self._extract_ocr(line, ocr_results)
+            self._extract_keywords(line, keyword_results)
+        return {'keywords': keyword_results, 'dates': dates_results, 'ocr': ocr_results}
 
     def search(self, keywords):
         result = dict((keyword, False) for keyword in keywords)
